223b98a80d8c4bedb9508a18fba622e973dd4240
Dexter Fitch <fitch.dexter@gmail.com>
2020-12-09 02:08:39 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const submitButton = document.getElementById("submit-button");
   const newTaskDescription = document.getElementById("new-task-description");
 
-  submitButton.addEventListener("click", function(submitButtonClick) {
+  submitButton.addEventListener("click", (submitButtonClick) => {
     let task = newTaskDescription.value;
     if (task === "") {
       alert("Blank tasks aren't real tasks.")
