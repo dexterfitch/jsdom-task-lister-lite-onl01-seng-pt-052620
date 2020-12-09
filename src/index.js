9b915068e5896efc7abb9c6fcac3e7e6c260914e
Dexter Fitch <fitch.dexter@gmail.com>
2020-12-09 02:22:32 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
       alert("Blank tasks aren't real tasks.")
     } else {
       taskCounter++;
-      document.getElementById("tasks").innerHTML += `<li id="task-${taskCounter}"><span class="delete-tasks" id="delete-task-${taskCounter}">x</span>  ${task}</li>`;
+      document.getElementById("tasks").innerHTML += `<li id="task-${taskCounter}"><span class="delete-tasks" id="delete-task-${taskCounter}">x</span> ${taskCounter}. ${task}</li>`;
       newTaskDescription.value = "";
     }
     submitButtonClick.preventDefault();
