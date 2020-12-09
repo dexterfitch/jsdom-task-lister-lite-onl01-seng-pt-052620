ce0bd79a4a7edcf11c486ec773d748fb8b30013d
Dexter Fitch <fitch.dexter@gmail.com>
2020-12-09 01:39:08 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const submitButton = document.getElementById("submit-button");
+  const newTaskDescription = document.getElementById("new-task-description");
+  
+  function parseTask() {
+    
+  }
+  
+  submitButton.addEventListener("click", function(submitButtonClick) {
+    submitButtonClick.preventDefault();
+    document.getElementById("tasks").innerHTML += `<li>${}</li>`;
+  });
 });
